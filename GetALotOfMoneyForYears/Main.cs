c3fac8301d778f411b749cee3ac695ceb0cf994a
Adpex <zmycoder@hotmail.com>
2019-07-08 18:02:12 +0800

    优化代码

--- GetALotOfMoneyForYears/Main.cs
+++ GetALotOfMoneyForYears/Main.cs
@@ -33,7 +33,7 @@ namespace GetALotOfMoneyForYears
 
         private void Txt_NowTime_TextChanged(object sender, EventArgs e)
         {
-            if (txt_NowTime.Text.Equals("15:30:00"))
+            if (txt_NowTime.Text.Equals("15:35:00"))
             {
                 GetDatas();
             }
@@ -79,44 +79,68 @@ namespace GetALotOfMoneyForYears
 
             JObject jo = JObject.Parse(result);
             JArray array = (JArray)jo["rows"];
-            
+
             for (int i = 0; i < array.Count; i++)
             {
-                
-                        DataRow dr = dt.NewRow();
-                        dr["债券ID"] = array[i]["cell"]["bond_id"];
-                        dr["债券名字"] = array[i]["cell"]["bond_nm"];
-                        dr["现价"] = array[i]["cell"]["price"];
-                        dr["涨跌幅"] = array[i]["cell"]["increase_rt"];
-                        dr["正股ID"] = array[i]["cell"]["pre_bond_id"];
-                        dr["正股名称"] = array[i]["cell"]["stock_nm"];
-                        dr["正股价"] = array[i]["cell"]["sprice"];
-                        dr["正股成交额"] = array[i]["cell"]["svolume"];
-                        dr["正股涨跌幅"] = array[i]["cell"]["sincrease_rt"];
-                        dr["PB"] = array[i]["cell"]["pb"];
-                        dr["转股价"] = array[i]["cell"]["convert_price"];
-                        dr["转股价值"] = array[i]["cell"]["convert_value"];
-                        dr["转股期提示"] = array[i]["cell"]["convert_cd_tip"];
-                        dr["溢价率"] = array[i]["cell"]["premium_rt"];
-                        dr["评级"] = array[i]["cell"]["rating_cd"];
-                        dr["回售触发价"] = array[i]["cell"]["put_convert_price"];
-                        dr["回售起始"] = array[i]["cell"]["next_put_dt"];
-                        dr["强赎触发价"] = array[i]["cell"]["force_redeem_price"];
-                        dr["转债占比"] = array[i]["cell"]["convert_amt_ratio"];
-                        dr["转债规模"] = array[i]["cell"]["orig_iss_amt"];
-                        dr["到期时间"] = array[i]["cell"]["short_maturity_dt"];
-                        dr["年限"] = array[i]["cell"]["year_left"];
-                        dr["到期税前收益"] = array[i]["cell"]["ytm_rt"];
-                        dr["到期税后收益"] = array[i]["cell"]["ytm_rt_tax"];
-                        dr["成交额"] = array[i]["cell"]["volume"];
-                        dt.Rows.Add(dr);
-                   
+                DataRow dr = dt.NewRow();
+                dr["债券代码"] = array[i]["cell"]["bond_id"];
+                dr["债券名称"] = array[i]["cell"]["bond_nm"];
+                double price = GetDoubles(array[i]["cell"]["price"].ToString().Trim());
+                dr["现价"] = price;
+                dr["涨跌幅（%）"] = GetDoubles(array[i]["cell"]["increase_rt"].ToString().Trim().TrimEnd('%'));
+                double premium_rt = GetDoubles(array[i]["cell"]["premium_rt"].ToString().Trim().TrimEnd('%'));
+                dr["溢价率（%）"] = premium_rt;
+                if(array[i]["cell"]["pre_bond_id"].ToString().Substring(0,2).Equals("sh"))
+                    dr["证交所"] = "沪";
+                else
+                    dr["证交所"] = "深";
+                dr["正股名称"] = array[i]["cell"]["stock_nm"];
+                dr["正股价"] = GetDoubles(array[i]["cell"]["sprice"].ToString().Trim());
+                dr["正股成交额（万）"] = GetDoubles(array[i]["cell"]["svolume"].ToString().Trim());
+                dr["正股涨跌幅（%）"] = GetDoubles(array[i]["cell"]["sincrease_rt"].ToString().Trim().TrimEnd('%'));
+                dr["PB"] = GetDoubles(array[i]["cell"]["pb"].ToString().Trim());
+                dr["转股价"] = GetDoubles(array[i]["cell"]["convert_price"].ToString().Trim());
+                dr["转股价值"] = GetDoubles(array[i]["cell"]["convert_value"].ToString().Trim());
+                string[] spStr = array[i]["cell"]["convert_cd_tip"].ToString().Split('；');
+                dr["转股代码"] = spStr[0];
+                dr["转股日期"] = Convert.ToDateTime(spStr[1].Substring(0, 10));
+                dr["评级"] = array[i]["cell"]["rating_cd"];
+                dr["回售触发价"] = GetDoubles(array[i]["cell"]["put_convert_price"].ToString().Trim().TrimEnd('%'));
+                string dates = GetDates(array[i]["cell"]["next_put_dt"].ToString().Trim());
+
+                if (!string.IsNullOrEmpty(dates))
+                {
+                    dr["回售起始"] = Convert.ToDateTime(dates);
+                    dr["距回售期月数"] = ((Convert.ToDateTime(dates).Year - DateTime.Now.Year) * 12) + Convert.ToDateTime(dates).Month - DateTime.Now.Month;
+                }
+                else
+                {
+                    dr["回售起始"] = DBNull.Value;
+                    dr["距回售期月数"] = "无回售条款";
+                }
+                dr["强赎触发价"] = GetDoubles(array[i]["cell"]["force_redeem_price"].ToString().Trim());
+                dr["转债占比（%）"] = GetDoubles(array[i]["cell"]["convert_amt_ratio"].ToString().Trim().TrimEnd('%'));
+                dr["转债规模（亿）"] = GetDoubles(array[i]["cell"]["orig_iss_amt"].ToString().Trim().TrimEnd('%'));
+                string shortdt = array[i]["cell"]["short_maturity_dt"].ToString().Trim();
+                if (!string.IsNullOrEmpty(shortdt))
+                    dr["到期时间"] = Convert.ToDateTime("20" + shortdt);
+                dr["年限"] = GetDoubles(array[i]["cell"]["year_left"].ToString().Trim());
+                dr["到期税前收益（%）"] = GetDoubles(array[i]["cell"]["ytm_rt"].ToString().Trim().TrimEnd('%'));
+                dr["到期税后收益（%）"] = GetDoubles(array[i]["cell"]["ytm_rt_tax"].ToString().Trim().TrimEnd('%'));
+                dr["成交额（万）"] = GetDoubles(array[i]["cell"]["volume"].ToString().Trim());
+                dr["系统价值"] = price + 3 * premium_rt;
+
+                dt.Rows.Add(dr);
+
             }
 
             //建立Excel对象
             Excel.Application excel = new Excel.Application();
             excel.Application.Workbooks.Add(true);
             excel.Visible = true;
+
+            //excel.SaveWorkspace(System.Windows.Forms.Application.StartupPath + string.Format(@"\Data\{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")));
+            //excel.Application.SaveWorkspace(System.Windows.Forms.Application.StartupPath + string.Format(@"\Data\{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")));
 
             //生成字段名称
             for (int i = 0; i < dt.Columns.Count; i++)
@@ -129,13 +153,25 @@ namespace GetALotOfMoneyForYears
             {
                 for (int j = 0; j < dt.Columns.Count; j++)
                 {
-                    if (dt.Rows[i][j].GetType() == typeof(string))
+                    //if (dt.Rows[i][j].GetType() == typeof(string))
+                    //{
+                    //    excel.Cells[i + 2, j + 1] = "'" + dt.Rows[i][j].ToString();
+                    //}
+                    //else
+                    //{
+                    //    excel.Cells[i + 2, j + 1] = dt.Rows[i][j].ToString();
+                    //}
+                    excel.Cells[i + 2, j + 1] = dt.Rows[i][j];
+                    if (dt.Columns[j] == dt.Columns["距回售期月数"])
                     {
-                        excel.Cells[i + 2, j + 1] = "'" + dt.Rows[i][j].ToString();
-                    }
-                    else
-                    {
-                        excel.Cells[i + 2, j + 1] = dt.Rows[i][j].ToString();
+                        string str = dt.Rows[i][j].ToString().Trim();
+                        double month = GetDoubles(dt.Rows[i][j].ToString());
+                        if (!str.Equals("无回售条款") && month < 24)
+                            excel.Cells[i + 2, j + 1].Interior.Color = Color.Yellow;
+                        if (!str.Equals("无回售条款") && month < 12)
+                            excel.Cells[i + 2, j + 1].Interior.Color = Color.Orange;
+                        if (!str.Equals("无回售条款") && month < 6)
+                            excel.Cells[i + 2, j + 1].Interior.Color = Color.Red;
                     }
                 }
             }
@@ -151,31 +187,53 @@ namespace GetALotOfMoneyForYears
         private void CreateDataTables()
         {
             DataColumnCollection dcc = dt.Columns;
-            dcc.Add("债券ID", typeof(String));//债券ID
-            dcc.Add("债券名字", typeof(String));//债券名字
-            dcc.Add("现价", typeof(String));//现价
-            dcc.Add("涨跌幅", typeof(String));//涨跌幅
-            dcc.Add("正股ID", typeof(String));//正股ID
+            dcc.Add("债券代码", typeof(String));//债券ID
+            dcc.Add("证交所", typeof(String));//证交所
             dcc.Add("正股名称", typeof(String));//正股名称
-            dcc.Add("正股价", typeof(String));//正股价
-            dcc.Add("正股成交额", typeof(String));//正股成交额
-            dcc.Add("正股涨跌幅", typeof(String));//正股涨跌幅
-            dcc.Add("PB", typeof(String));//PB
-            dcc.Add("转股价", typeof(String));//转股价
-            dcc.Add("转股价值", typeof(String));//转股价值
-            dcc.Add("转股期提示", typeof(String));//转股期提示
-            dcc.Add("溢价率", typeof(String));//溢价率
+            dcc.Add("债券名称", typeof(String));//债券名字
+            dcc.Add("现价", typeof(Double));//现价
+            dcc.Add("溢价率（%）", typeof(Double));//溢价率
+            dcc.Add("系统价值", typeof(Double));//算法：现价+300*溢价率
+            dcc.Add("涨跌幅（%）", typeof(Double));//涨跌幅
+            dcc.Add("正股价", typeof(Double));//正股价
+            dcc.Add("正股涨跌幅（%）", typeof(Double));//正股涨跌幅
+            dcc.Add("成交额（万）", typeof(Double));//成交额
+            dcc.Add("正股成交额（万）", typeof(Double));//正股成交额
+            dcc.Add("PB", typeof(Double));//PB
+            dcc.Add("转股价", typeof(Double));//转股价
+            dcc.Add("转股价值", typeof(Double));//转股价值
             dcc.Add("评级", typeof(String));//评级
-            dcc.Add("回售触发价", typeof(String));//回售触发价
-            dcc.Add("回售起始", typeof(String));//回售起始
-            dcc.Add("强赎触发价", typeof(String));//强赎触发价
-            dcc.Add("转债占比", typeof(String));//转债占比
-            dcc.Add("转债规模", typeof(String));//转债规模
-            dcc.Add("到期时间", typeof(String));//到期时间
-            dcc.Add("年限", typeof(String));//年限
-            dcc.Add("到期税前收益", typeof(String));//到期税前收益
-            dcc.Add("到期税后收益", typeof(String));//到期税后收益
-            dcc.Add("成交额", typeof(String));//成交额
+            dcc.Add("回售触发价", typeof(Double));//回售触发价
+            dcc.Add("回售起始", typeof(DateTime));//回售起始
+            dcc.Add("距回售期月数", typeof(String));//回售起始
+            dcc.Add("强赎触发价", typeof(Double));//强赎触发价
+            dcc.Add("转股代码", typeof(String));
+            dcc.Add("转股日期", typeof(DateTime));//转股期提示
+            dcc.Add("转债占比（%）", typeof(String));//转债占比
+            dcc.Add("转债规模（亿）", typeof(String));//转债规模
+            dcc.Add("到期时间", typeof(DateTime));//到期时间
+            dcc.Add("年限", typeof(Double));//年限
+            dcc.Add("到期税前收益（%）", typeof(Double));//到期税前收益
+            dcc.Add("到期税后收益（%）", typeof(Double));//到期税后收益
+            
+        }
+
+        private double GetDoubles(string str)
+        {
+            double result = -1;
+            double.TryParse(str, out result);
+            return result;
+        }
+
+        private string GetDates(string str)
+        {
+            string result = string.Empty;
+            DateTime dt = new DateTime();
+            DateTime.TryParse(str, out dt);
+            if (dt.Year != 1)
+                result = dt.ToString("yyyy-MM-dd");
+
+            return result;
         }
     }
 }
